d1d13078967a5ef6edf97d8ac9be10336f49cd6c
Rory Hackney <roryhackney@gmail.com>
2025-10-04 14:48:30 -0700

    final submission

--- public/test.js
+++ public/test.js
@@ -1,19 +1,19 @@
 
-async function query(data) {
+async function query() {
     const response = await fetch(
         "https://cloud.flowiseai.com/api/v1/prediction/eeca997d-cff7-49c8-ba49-4f080bf24eea",
         {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
+            }
         }
     );
     const result = await response.json();
     return result;
 }
 
-query({"question": "Hey, how are you?"}).then((response) => {
+query().then((response) => {
     console.log(response);
+    document.getElementById("result").innerHTML = "<p>" + response.text + "</p>";
 });
